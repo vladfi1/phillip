4b7c0ed30d1858b50f75b00984ff81d5d144a5b7
Vlad Firoiu <vladfi2@gmail.com>
2016-04-10 19:52:52 -0400

    Added policy network. Currently can't train because of a tf error when passing the actor variables to be optimized.
    See https://github.com/tensorflow/tensorflow/issues/1841

--- RL.py
+++ RL.py
@@ -21,7 +21,9 @@ def inputCType(ctype, shape=None, name=""):
   else:
     raise TypeError("Unhandled ctype " + ctype)
 
-def feedCTypes(ctype, input, values, feed_dict={}):
+def feedCTypes(ctype, input, values, feed_dict=None):
+  if feed_dict is None:
+    feed_dict = {}
   if ctype in ctypes2TF:
     feed_dict[input] = values
   elif issubclass(ctype, ctypes.Structure):
@@ -57,31 +59,32 @@ actionHelper = tfl.makeAffineLayer(maxAction, actionSpace)
 def embedAction(t):
   return actionHelper(one_hot(maxAction)(t))
 
-playerEmbedding = {
-  "percent" : castFloat,
-  "facing" : castFloat,
-  "x" : embedFloat,
-  "y" : embedFloat,
-  "action" : embedAction,
-  "action_counter" : castFloat,
-  "action_frame" : castFloat,
-  "character" : one_hot(maxCharacter),
-  "invulnerable" : castFloat,
-  "hitlag_frames_left" : castFloat,
-  "hitstun_frames_left" : castFloat,
-  "jumps_left" : castFloat,
-  "charging_smash" : castFloat,
-  "on_ground" : castFloat,
-  'speed_air_x_self' :  embedFloat,
-  'speed_ground_x_self' : embedFloat,
-  'speed_y_self' : embedFloat,
-  'speed_x_attack' : embedFloat,
-  'speed_y_attack' : embedFloat
-}
+playerEmbedding = [
+  ("percent", castFloat),
+  ("facing", castFloat),
+  ("x", embedFloat),
+  ("y", embedFloat),
+  ("action", embedAction),
+  ("action_counter", castFloat),
+  ("action_frame", castFloat),
+  ("character", one_hot(maxCharacter)),
+  ("invulnerable", castFloat),
+  ("hitlag_frames_left", castFloat),
+  ("hitstun_frames_left", castFloat),
+  ("jumps_left", castFloat),
+  ("charging_smash", castFloat),
+  ("on_ground", castFloat),
+  ('speed_air_x_self',  embedFloat),
+  ('speed_ground_x_self', embedFloat),
+  ('speed_y_self', embedFloat),
+  ('speed_x_attack', embedFloat),
+  ('speed_y_attack', embedFloat)
+]
 
+# TODO: give the tensors some names/scopes
 def embedStruct(embedding):
   def f(struct):
-    embed = [op(struct[field]) for field, op in embedding.items()]
+    embed = [op(struct[field]) for field, op in embedding]
     return tf.concat(1, embed)
   return f
 
@@ -95,51 +98,81 @@ stageHelper = tfl.makeAffineLayer(maxStage, stageSpace)
 def embedStage(stage):
   return stageHelper(one_hot(maxStage)(stage))
 
-gameEmbedding = {
-  'player_one' : embedPlayer,
-  'player_two' : embedPlayer,
+gameEmbedding = [
+  ('player_one', embedPlayer),
+  ('player_two', embedPlayer),
 
   #('frame', c_uint),
-  'stage' : embedStage
-}
+  ('stage', embedStage)
+]
 
 embedGame = embedStruct(gameEmbedding)
 embedded_states = embedGame(input_states)
+state_size = embedded_states.get_shape()[-1].value
 
-controllerEmbedding = {
-  'buttonA' : castFloat,
-  'buttonB' : castFloat,
-  'buttonX' : castFloat,
-  'buttonY' : castFloat,
-  'buttonL' : castFloat,
-  'buttonR' : castFloat,
+controllerEmbedding = [
+  ('buttonA', castFloat),
+  ('buttonB', castFloat),
+  ('buttonX', castFloat),
+  ('buttonY', castFloat),
+  ('buttonL', castFloat),
+  ('buttonR', castFloat),
   
-  'analogL' : embedFloat,
-  'analogR' : embedFloat,
+  ('analogL', embedFloat),
+  ('analogR', embedFloat),
   
-  'mainX' : embedFloat,
-  'mainY' : embedFloat,
+  ('mainX', embedFloat),
+  ('mainY', embedFloat),
     
-  'cX' : embedFloat,
-  'cY' : embedFloat
-}
+  ('cX', embedFloat),
+  ('cY', embedFloat)
+]
 
 embedController = embedStruct(controllerEmbedding)
 embedded_controls = embedController(input_controls)
+control_size = embedded_controls.get_shape()[-1].value
+assert(control_size == 12)
 
-state_actions = tf.concat(1, [embedded_states, embedded_controls])
+with tf.variable_scope("qNetwork"):
+  q1 = tfl.makeAffineLayer(state_size + control_size, 512, tf.tanh)
+  q2 = tfl.makeAffineLayer(512, 1)
 
-q1 = tfl.affineLayer(state_actions, 512, tf.tanh)
-q = tfl.affineLayer(q1, 1)
-q = tf.squeeze(q)
+def q(states, controls):
+  state_actions = tf.concat(1, [embedded_states, embedded_controls])
+  return tf.squeeze(q2(q1(state_actions)))
+
+qPredictions = q(embedded_states, embedded_controls)
 
 # pre-computed long-term rewards
 rewards = tf.placeholder(tf.float32, [None], name='rewards')
 
-qLosses = tf.squared_difference(q, rewards)
+qLosses = tf.squared_difference(qPredictions, rewards)
 qLoss = tf.reduce_sum(qLosses)
 
-trainQ = tf.train.RMSPropOptimizer(0.001).minimize(qLoss)
+trainQ = tf.train.RMSPropOptimizer(0.0001).minimize(qLoss)
+
+with tf.variable_scope("actor"):
+  applyC1 = tfl.makeAffineLayer(state_size, 64, tf.tanh)
+  # the first 8 members of controllerEmbedding are in [0,1]
+  applyC2Sigmoid = tfl.makeAffineLayer(64, 8, tf.sigmoid)
+  # the last 4 are in [-1, 1]
+  applyC2Tanh = tfl.makeAffineLayer(64, 4, tf.tanh)
+
+actor_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor')
+#print(actor_variables)
+
+def actor(states):
+  c1 = applyC1(states)
+  c2Sigmoid = applyC2Sigmoid(c1)
+  c2Tanh = applyC2Tanh(c1)
+  return tf.concat(1, [c2Sigmoid, c2Tanh])
+
+actions = actor(embedded_states)
+actorQ = tf.reduce_sum(q(embedded_states, actions))
+
+#trainActor = tf.train.RMSPropOptimizer(0.001).minimize(-actorQ)
+trainActor = tf.train.RMSPropOptimizer(0.001).minimize(-actorQ, var_list=actor_variables)
+#trainActor = tf.train.RMSPropOptimizer(0.001).minimize(-actorQ, var_list=tf.trainable_variables())
 
 # see GameState.h for explanations
 dyingActions = set([0x0, 0x1, 0x2, 0x4, 0x6, 0x7, 0x8])
@@ -147,12 +180,21 @@ dyingActions = set([0x0, 0x1, 0x2, 0x4, 0x6, 0x7, 0x8])
 def isDying(player):
   return player.action in dyingActions
 
-# from player 2's perspective
-def score(state):
-  return isDying(state.player_one) - isDying(state.player_two)
+# players tend to be dead for many frames in a row
+# here we prune a all but the first frame of the death
+def processDeaths(deaths):
+  return util.zipWith(lambda prev, next: (not prev) and next, [False] + deaths[:-1] , deaths)
 
+# from player 2's perspective
 def computeRewards(states, discount = 0.99):
-  scores = map(score, states)
+  kills = [isDying(state.player_one) for state in states]
+  deaths = [isDying(state.player_two) for state in states]
+  
+  kills = processDeaths(kills)
+  deaths = processDeaths(deaths)
+  
+  scores = util.zipWith(lambda x, y: x - y, kills, deaths)
+  
   return util.scanr1(lambda r1, r2: r1 + discount * r2, scores)
   
 def readFile(filename, states=None, controls=None):
@@ -174,21 +216,26 @@ def readFile(filename, states=None, controls=None):
   
   return states, controls
 
+saver = tf.train.Saver(tf.all_variables())
+
 sess = tf.Session()
 
-def computeQLoss(filename):
+def train(filename):
   states, controls = readFile(filename)
   
   feed_dict = {rewards : computeRewards(states)}
-  feedCTypes(ssbm.GameMemory, input_states, states, feed_dict)
+  feedCTypes(ssbm.GameMemory, input_states, states, feed_dict)  
   feedCTypes(ssbm.ControllerState, input_controls, controls, feed_dict)
   
   print(sess.run(qLoss, feed_dict))
   
   for _ in range(10):
-    sess.run(trainQ, feed_dict)
-    print(sess.run(qLoss, feed_dict))
+    for _ in range(10):
+      sess.run([trainQ, trainActor], feed_dict)
+    print(sess.run([qLoss, actorQ], feed_dict))
+  
+  saver.save(sess, 'simpleDQN')
 
 sess.run(tf.initialize_all_variables())
-computeQLoss('testRecord0')
+train('testRecord0')
 
