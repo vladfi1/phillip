7b50a35f65f91ee04591b68e48d4c2f5bdf3b959
Vlad Firoiu <vladfi2@gmail.com>
2016-04-14 15:59:19 -0400

    Use namespace std;

--- MemoryWatcher.cpp
+++ MemoryWatcher.cpp
@@ -15,27 +15,29 @@
 
 #include "MemoryWatcher.h"
 
+using namespace std;
+
 // TODO: pass in watcher location
 MemoryWatcher::MemoryWatcher()
 {
     struct passwd *pw = getpwuid(getuid());
-    std::string home_path = std::string(pw->pw_dir);
-    std::string legacy_config_path = home_path + "/.dolphin-emu";
-    std::string mem_watcher_path;
+    string home_path = string(pw->pw_dir);
+    string legacy_config_path = home_path + "/.dolphin-emu";
+    string mem_watcher_path;
 
     struct stat buffer;
     if(stat(legacy_config_path.c_str(), &buffer) != 0)
     {
         //If the legacy app path is not present, see if the new one is
-        const char *env_XDG_DATA_HOME = std::getenv("XDG_DATA_HOME");
+        const char *env_XDG_DATA_HOME = getenv("XDG_DATA_HOME");
         if(env_XDG_DATA_HOME == NULL)
         {
             //Try $HOME/.local/share next
-            std::string backup_path = home_path + "/.local/share/dolphin-emu";
+            string backup_path = home_path + "/.local/share/dolphin-emu";
             if(stat(backup_path.c_str(), &buffer) != 0)
             {
-                std::cout << "ERROR: $XDG_DATA_HOME was empty and so was $HOME/.dolphin-emu and $HOME/.local/share/dolphin-emu " \
-                    "Are you sure Dolphin is installed? Make sure it is, and then run the CPU again." << std::endl;
+                cout << "ERROR: $XDG_DATA_HOME was empty and so was $HOME/.dolphin-emu and $HOME/.local/share/dolphin-emu " \
+                    "Are you sure Dolphin is installed? Make sure it is, and then run the CPU again." << endl;
                 exit(-1);
             }
             mem_watcher_path = backup_path;
@@ -186,7 +188,7 @@ void ReadPlayer(PlayerMemory& player, uint ptr_int, uint value_int)
         }
         default:
         {
-            std::cout << "WARNING: Got an unexpected memory pointer: " << ptr_int << std::endl;
+            cout << "WARNING: Got an unexpected memory pointer: " << ptr_int << endl;
         }
     }
 }
@@ -199,21 +201,21 @@ bool MemoryWatcher::ReadMemory(GameMemory& memory)
     struct sockaddr remaddr;
     socklen_t addr_len;
     recvfrom(m_file, buf, sizeof(buf), 0, &remaddr, &addr_len);
-    std::stringstream ss(buf);
-    std::string region, value;
+    stringstream ss(buf);
+    string region, value;
 
-    std::getline(ss, region, '\n');
-    std::getline(ss, value, '\n');
-    uint value_int = std::stoul(value.c_str(), nullptr, 16);
+    getline(ss, region, '\n');
+    getline(ss, value, '\n');
+    uint value_int = stoul(value.c_str(), nullptr, 16);
 
     //Is this a followed pointer?
-    std::size_t found = region.find(" ");
-    if(found != std::string::npos)
+    size_t found = region.find(" ");
+    if(found != string::npos)
     {
-        std::string ptr = region.substr(found+1);
-        std::string base = region.substr(0, found);
-        uint ptr_int = std::stoul(ptr.c_str(), nullptr, 16);
-        uint base_int = std::stoul(base.c_str(), nullptr, 16);
+        string ptr = region.substr(found+1);
+        string base = region.substr(0, found);
+        uint ptr_int = stoul(ptr.c_str(), nullptr, 16);
+        uint base_int = stoul(base.c_str(), nullptr, 16);
 
         switch(base_int)
         {
@@ -231,14 +233,14 @@ bool MemoryWatcher::ReadMemory(GameMemory& memory)
             }
             default:
             {
-                std::cout << "WARNING: Got an unexpected memory base pointer: " << base_int << std::endl;
+                cout << "WARNING: Got an unexpected memory base pointer: " << base_int << endl;
             }
         }
     }
     //If not, it's a direct pointer
     else
     {
-        uint region_int = std::stoul(region.c_str(), nullptr, 16);
+        uint region_int = stoul(region.c_str(), nullptr, 16);
         switch(region_int)
         {
             //Frame
@@ -325,12 +327,14 @@ bool MemoryWatcher::ReadMemory(GameMemory& memory)
             case 0x479d30:
             {
                 memory.menu_state = value_int;
+                cout << value_int << endl;
                 break;
             }
             //Stage
             case 0x4D6CAD:
             {
                 memory.stage = value_int >> 16;
+                cout << value_int << endl;
                 break;
             }
             //p2 cursor x
@@ -347,7 +351,7 @@ bool MemoryWatcher::ReadMemory(GameMemory& memory)
             }
             case 0x003F0E08:
             {
-                std::cout << std::hex << "P1: " << value_int << std::endl;
+                cout << hex << "P1: " << value_int << endl;
                 break;
             }
         }
