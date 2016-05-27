2bbdf67b05493790dab8fe2cc35c97ee0e12ab8a
Vlad Firoiu <vladfi2@gmail.com>
2016-05-27 17:35:15 -0400

    indentation

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ import time
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("--debug", action="store_true",
-                   help="set debug breakpoint")
+           help="set debug breakpoint")
 parser.add_argument("-q", "--quiet", action="store_true",
-                   help="don't print status messages to stdout")
+           help="don't print status messages to stdout")
 parser.add_argument("--init", action="store_true",
-                   help="initialize variables")
+           help="initialize variables")
 parser.add_argument("--path", help="where to import from and save to")
 parser.add_argument("--name", type=str, help="sets path to saves/{name}")
 parser.add_argument("--model", choices=["DQN", "ActorCritic", "ThompsonDQN"], required=True, help="which RL model to use")
@@ -42,35 +42,35 @@ model = RL.Model(mode=RL.Mode.TRAIN, **args.__dict__)
 
 # do this in RL?
 if args.init:
-    model.init()
-    model.save()
+  model.init()
+  model.save()
 else:
-    model.restore()
+  model.restore()
 
 def sweep(data_dir='experience/'):
-    i = 0
-    rewards = []
-    start_time = time.time()
-    for f in os.listdir(data_dir):
-        if not (f.startswith(".") or f.startswith("tmp")): # .DS_Store, temp files
-            filename = data_dir + f
-            if not os.path.exists(filename):
-                continue
-            print("Step", i)
-            print("Experience " + filename)
-            rewards.append(model.train(filename))
-            i += 1
-        else:
-            print("Not training on file:", f)
-        print("")
-    if i > 0:
-        total_time = time.time() - start_time
-        print("time/experience", total_time / i)
-    model.save()
-    #RL.writeGraph()
-    # import pdb; pdb.set_trace()
-    mean_reward = sum(rewards) / len(rewards) if len(rewards) > 0 else 0
-    return mean_reward
+  i = 0
+  rewards = []
+  start_time = time.time()
+  for f in os.listdir(data_dir):
+    if not (f.startswith(".") or f.startswith("tmp")): # .DS_Store, temp files
+      filename = data_dir + f
+      if not os.path.exists(filename):
+        continue
+      print("Step", i)
+      print("Experience " + filename)
+      rewards.append(model.train(filename))
+      i += 1
+    else:
+      print("Not training on file:", f)
+    print("")
+  if i > 0:
+    total_time = time.time() - start_time
+    print("time/experience", total_time / i)
+  model.save()
+  #RL.writeGraph()
+  # import pdb; pdb.set_trace()
+  mean_reward = sum(rewards) / len(rewards) if len(rewards) > 0 else 0
+  return mean_reward
 
 while True:
-    sweep(experience_dir)
+  sweep(experience_dir)
