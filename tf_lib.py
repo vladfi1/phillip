38eb33527ef88ca80351ce646c3b56d53974e77d
Vlad Firoiu <vladfi2@gmail.com>
2016-06-04 02:04:29 -0400

    nicer tf runner

--- tf_lib.py
+++ tf_lib.py
@@ -203,3 +203,27 @@ def one_hot(size):
       size,
       1.0,
       0.0)
+
+def run(session, fetches, feed_dict):
+    """Wrapper for making Session.run() more user friendly.
+
+    With this function, fetches can be either a list or a dictionary.
+
+    If fetches is a list, this function will behave like
+    tf.session.run() and return a list in the same order as well. If
+    fetches is a dict then this function will also return a dict where
+    the returned values are associated with the corresponding keys from
+    the fetches dict.
+
+    Keyword arguments:
+    session -- An open TensorFlow session.
+    fetches -- A list or dict of ops to fetch.
+    feed_dict -- The dict of values to feed to the computation graph.
+    """
+    if isinstance(fetches, dict):
+        keys, values = fetches.keys(), list(fetches.values())
+        res = session.run(values, feed_dict)
+        return {key: value for key, value in zip(keys, res)}
+    else:
+        return session.run(fetches, feed_dict)
+
