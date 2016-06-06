5f250eff7f52d89a3c119169088658b8d4c5a758
Vlad Firoiu <vladfi2@gmail.com>
2016-06-06 04:19:57 -0400

    runner overhaul

--- runner.py
+++ runner.py
@@ -4,7 +4,6 @@ import sys
 dry_run = '--dry-run' in sys.argv
 local   = '--local' in sys.argv
 detach  = '--detach' in sys.argv
-agents = '--agents' in sys.argv
 
 if not os.path.exists("slurm_logs"):
     os.makedirs("slurm_logs")
@@ -12,170 +11,102 @@ if not os.path.exists("slurm_logs"):
 if not os.path.exists("slurm_scripts"):
     os.makedirs("slurm_scripts")
 
+exp_name = "exp"
+job_flags = dict(train="", agent="")
+job_dicts = dict(train={}, agent={})
 
-train_params = {
-  'init': True,
-}
+def add_param(param, value, jobs, name=True):
+  global exp_name
+  if isinstance(value, bool):
+    if value:
+      flag = " --" + param
+      for job in jobs:
+        job_flags[job] += flag
+        job_dicts[job][param] = value
+      if name:
+        exp_name += "_" + param
+  else:
+    flag = " --" + param + " " + str(value)
+    for job in jobs:
+      job_flags[job] += flag
+      job_dicts[job][param] = value
+    if name:
+      exp_name += "_" + param + "_" + str(value)
 
-model = 'ActorCritic'
-#model = 'DQN'
-name = model
-train_params['model'] = model
+both = ['train', 'agent']
 
-movie = 'Falcon9Falcon'
-#name += '_' + movie
+init = True
+add_param('model', 'ActorCritic', both)
+add_param('epsilon', 0.02, both)
 
-learning_rate = 1e-3
-train_params['learning_rate'] = learning_rate
-name += '_' + str(learning_rate)
+train_settings = [
+  ('learning_rate', 0.001),
+  ('tdN', 5),
+  ('batch_size', 10),
+  ('batch_steps', 1),
+  #('sarsa', True'),
+  #('target_delay', 5000),
+  ('entropy_scale', 0.07),
+]
 
-tdN = 5
-train_params['tdN'] = tdN
-name += '_' + str(tdN)
+for k, v in train_settings:
+  add_param(k, v, ['train'])
 
-batch_size = 100
-train_params['batch_size'] = batch_size
-name += '_' + str(batch_size)
+# agent settings
 
-batch_steps = 1
-train_params['batch_steps'] = batch_steps
-name += '_' + str(batch_steps)
+add_param('dolphin', True, ['agent'], False)
 
-if model.endswith('DQN'):
-  sarsa = False
-  #sarsa = True
-  train_params['sarsa'] = sarsa
-  if sarsa:
-    name += '_sarsa'
+self_play = False
+movie = 'FalconFalcon' if self_play else 'Falcon9Falcon'
+movie += '.dtm'
 
-  target_delay = None
-  target_delay = 5000
-  train_params['target_delay'] = target_delay
-  if target_delay:
-    name += '_' + str(target_delay)
-elif model == 'ActorCritic':
-  entropy_scale = 0.005
-  train_params['entropy_scale'] = entropy_scale
-  name += '_' + str(entropy_scale)
+add_param('self_play', self_play, ['agent'])
+add_param('movie', movie, ['agent'], False)
+add_param('dump_max', 10, ['agent'])
 
-train_params['name'] = name
+agents = 10
+add_param('agents', agents, [])
 
-trainer_jobs = [train_params]
-if agents:
-  trainer_jobs=[]
+#add_param('name', exp_name, both, False)
+add_param('path', "saves/%s/" % exp_name, both, False)
 
-agent_jobs = []
+def slurm_script(name, command, gpu=False):
+  slurmfile = 'slurm_scripts/' + name + '.slurm'
+  with open(slurmfile, 'w') as f:
+    f.write("#!/bin/bash\n")
+    f.write("#SBATCH --job-name"+"=" + name + "\n")
+    f.write("#SBATCH --output=slurm_logs/" + name + ".out\n")
+    f.write("#SBATCH --error=slurm_logs/" + name + ".err\n")
+    if gpu:
+      f.write("#SBATCH --gres=gpu:1\n")
+    f.write(command)
 
-n_agents = 50
-if not agents:
-  n_agents = 0
-for _ in range(n_agents):
-    exemplar = {
-            'model': model,
-            'movie': movie + '.dtm',
-            'dump_max': 10,
-            'dolphin': True,
-            'self_play': False,
-            'name': name,
-        }
-    agent_jobs.append(exemplar)
-
+  if dry_run:
+    print(command)
+  else:
+    os.system("sbatch " + slurmfile)
+    #os.system("sbatch -N 1 -c 2 --mem=8000 --time=6-23:00:00 slurm_scripts/" + jobname + ".slurm &")
 
 if dry_run:
-    print("NOT starting jobs:")
+  print("NOT starting jobs:")
 else:
-    print("Starting jobs:")
+  print("Starting jobs:")
 
-for i, job in enumerate(trainer_jobs):
-    jobname = "trainer_" + str(i)
-    flagstring = ""
-    for flag in job:
-        if isinstance(job[flag], bool):
-            if job[flag]:
-                jobname = jobname + "_" + flag
-                flagstring = flagstring + " --" + flag
-            else:
-                print("WARNING: Excluding 'False' flag " + flag)
-        # elif flag == 'import':
-        #     imported_network_name = job[flag]
-        #     if imported_network_name in base_networks.keys():
-        #         network_location = base_networks[imported_network_name]
-        #         jobname = jobname + "_" + flag + "_" + str(imported_network_name)
-        #         flagstring = flagstring + " --" + flag + " " + str(network_location)
-        #     else:
-        #         jobname = jobname + "_" + flag + "_" + str(job[flag])
-        #         flagstring = flagstring + " --" + flag + " " + networks_prefix + "/" + str(job[flag])
-        else:
-            jobname = jobname + "_" + flag + "_" + str(job[flag])
-            flagstring = flagstring + " --" + flag + " " + str(job[flag])
-    # flagstring = flagstring + " --name " + jobname
+  # init model for the first time
+  if init:
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    import RL
+    model = RL.Model(mode=RL.Mode.TRAIN, **job_dicts['train'])
+    model.init()
+    model.save()
 
-    jobcommand = "python3 -u train.py" + flagstring
+train_name = "trainer_" + exp_name
+train_command = "python3 -u train.py" + job_flags['train']
 
-    print(jobcommand)
-    if local and not dry_run:
-        if detach:
-            os.system(jobcommand + ' 2> slurm_logs/' + jobname + '.err 1> slurm_logs/' + jobname + '.out &')
-        else:
-            os.system(jobcommand)
+slurm_script(train_name, train_command)
 
-    else:
-        with open('slurm_scripts/' + jobname + '.slurm', 'w') as slurmfile:
-            slurmfile.write("#!/bin/bash\n")
-            slurmfile.write("#SBATCH --job-name"+"=" + jobname + "\n")
-            slurmfile.write("#SBATCH --output=slurm_logs/" + jobname + ".out\n")
-            slurmfile.write("#SBATCH --error=slurm_logs/" + jobname + ".err\n")
-            slurmfile.write(jobcommand)
+agent_command = "python3 -u run.py" + job_flags['agent']
+for i in range(agents):
+  agent_name = "agent_%d_%s" % (i, exp_name)
+  slurm_script(agent_name, agent_command)
 
-        if not dry_run:
-            # if 'gpu' in job and job['gpu']:
-            os.system("sbatch --gres=gpu:1 slurm_scripts/" + jobname + ".slurm")
-            # else:
-            #     os.system("sbatch -N 1 -c 2 --mem=8000 --time=6-23:00:00 slurm_scripts/" + jobname + ".slurm &")
-
-for i, job in enumerate(agent_jobs):
-    jobname = "agent_" + str(i)
-    flagstring = ""
-    for flag in job:
-        if isinstance(job[flag], bool):
-            if job[flag]:
-                jobname = jobname + "_" + flag
-                flagstring = flagstring + " --" + flag
-            else:
-                print("WARNING: Excluding 'False' flag " + flag)
-        # elif flag == 'import':
-        #     imported_network_name = job[flag]
-        #     if imported_network_name in base_networks.keys():
-        #         network_location = base_networks[imported_network_name]
-        #         jobname = jobname + "_" + flag + "_" + str(imported_network_name)
-        #         flagstring = flagstring + " --" + flag + " " + str(network_location)
-        #     else:
-        #         jobname = jobname + "_" + flag + "_" + str(job[flag])
-        #         flagstring = flagstring + " --" + flag + " " + networks_prefix + "/" + str(job[flag])
-        else:
-            jobname = jobname + "_" + flag + "_" + str(job[flag])
-            flagstring = flagstring + " --" + flag + " " + str(job[flag])
-    # flagstring = flagstring + " --name " + jobname
-
-    jobcommand = "python3 -u run.py" + flagstring
-
-    print(jobcommand)
-    if local and not dry_run:
-        if detach:
-            os.system(jobcommand + ' 2> slurm_logs/' + jobname + '.err 1> slurm_logs/' + jobname + '.out &')
-        else:
-            os.system(jobcommand)
-
-    else:
-        with open('slurm_scripts/' + jobname + '.slurm', 'w') as slurmfile:
-            slurmfile.write("#!/bin/bash\n")
-            slurmfile.write("#SBATCH --job-name"+"=" + jobname + "\n")
-            slurmfile.write("#SBATCH --output=slurm_logs/" + jobname + ".out\n")
-            slurmfile.write("#SBATCH --error=slurm_logs/" + jobname + ".err\n")
-            slurmfile.write(jobcommand)
-
-        if not dry_run:
-            # if 'gpu' in job and job['gpu']:
-            #     os.system("sbatch -c 2 --gres=gpu:titan-x:1 --mem=8000 --time=6-23:00:00 slurm_scripts/" + jobname + ".slurm &")
-            # else:
-            os.system("sbatch slurm_scripts/" + jobname + ".slurm &")
