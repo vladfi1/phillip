Fix memory leak in kl-based policy_scale adjustment.

RL.py
@@ -234,17 +234,12 @@ class Model:
         
         kl = np.mean(np.sum((np.exp(new_logp) - np.exp(old_logp)) * (new_logp - old_logp), -1))
         
-        min_rate = 1e-8
-        max_rate = 1e0
-        
-        policy_scale = self.model.policy_scale
-        
         if kl > target_kl * 2:
           print("kl too high")
-          self.sess.run(tf.assign(policy_scale, tf.maximum(min_rate, policy_scale / 1.5)))
+          self.sess.run(self.model.decrease_policy_scale)
         elif kl < target_kl / 2:
           print("kl too low")
-          self.sess.run(tf.assign(policy_scale, tf.minimum(max_rate, policy_scale * 1.5)))
+          self.sess.run(self.model.increase_policy_scale)
         else:
           print("kl just right!")
       

actor_critic_split.py
@@ -64,13 +64,19 @@ class ActorCriticSplit:
     
     self.policy_scale = tf.Variable(policy_scale)
     
+    min_rate = 1e-8
+    max_rate = 1e2
+    
+    self.decrease_policy_scale = tf.assign(self.policy_scale, tf.maximum(min_rate, self.policy_scale / 1.5))
+    self.increase_policy_scale = tf.assign(self.policy_scale, tf.minimum(max_rate, self.policy_scale * 1.5))
+    
     acLoss = vLoss - self.policy_scale * (actor_gain + entropy_scale * actor_entropy)
     
     stats = [
       ('vLoss', vLoss),
       ('actor_gain', actor_gain),
       ('actor_entropy', actor_entropy),
-      ('policy_scale', self.policy_scale)
+      ('policy_scale', tf.log(self.policy_scale))
     ]
 
     return acLoss, stats, log_actor_probs