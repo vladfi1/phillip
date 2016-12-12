99bfd6c303af932aa125e1395e32aaf809022080
Vlad Firoiu <vladfi2@gmail.com>
2016-12-12 14:03:49 -0500

    delay 1 and delay 2 agents

--- runner.py
+++ runner.py
@@ -8,6 +8,11 @@ exp_name = "diagonal"
 job_flags = dict(train="", agent="")
 job_dicts = dict(train=OrderedDict(), agent=OrderedDict())
 
+def toStr(val):
+  if isinstance(val, list):
+    return "_".join(map(str, val))
+  return str(val)
+
 def add_param(param, value, jobs, name=True):
   global exp_name
   if isinstance(value, bool):
@@ -19,19 +24,19 @@ def add_param(param, value, jobs, name=True):
       if name:
         exp_name += "_" + param
   else:
-    flag = " --" + param + " " + str(value)
+    flag = " --" + param + " " + toStr(value)
     for job in jobs:
       job_flags[job] += flag
       job_dicts[job][param] = value
     if name:
-      exp_name += "_" + param + "_" + str(value)
+      exp_name += "_" + param + "_" + toStr(value)
 
 both = ['train', 'agent']
 
 #model = 'DQN'
 #model = 'NaturalDQN'
-model = 'ActorCritic'
-#model = 'RecurrentActorCritic'
+#model = 'ActorCritic'
+model = 'RecurrentActorCritic'
 #model = 'NaturalActorCritic'
 
 recurrent = model.count('Recurrent')
@@ -44,13 +49,13 @@ train_settings = [
   ('tdN', 6),
   ('sweeps', 1),
   ('batches', 20),
-  ('batch_size', 50 if recurrent else 200),
+  ('batch_size', 100 if recurrent else 500),
   ('batch_steps', 1),
   ('gpu', 1),
 ]
 
 natural = True
-natural = False
+#natural = False
 
 if model.count('DQN'):
   train_settings += [
@@ -60,46 +65,57 @@ if model.count('DQN'):
   add_param('temperature', 0.0002, ['agent'], True)
 elif model.count('ActorCritic'):
   if natural:
-    add_param('policy_scale', 1, ['train'], True)
-    add_param('entropy_scale', 5e-4, ['train'], True)
+    add_param('policy_scale', 5, ['train'], True)
+    add_param('entropy_scale', 1e-3, ['train'], True)
   else:
-    add_param('policy_scale', 1, ['train'], True)
-    add_param('entropy_scale', 2e-3, ['train'], True)
+    add_param('policy_scale', 5 if recurrent else 1, ['train'], True)
+    add_param('entropy_scale', 1e-3 if recurrent else 2e-3, ['train'], True)
   #add_param('target_kl', 1e-5, ['train'], True)
 
 if natural:
   add_param('natural', True, ['train'], True)
   if model.count('ActorCritic'):
-    add_param('kl_scale', 0.1, ['train'], True)
+    add_param('kl_scale', 0.2, ['train'], True)
     
   if True:
-    add_param('target_distance', 5e-5, ['train'], True)
+    add_param('target_distance', 1e-5, ['train'], True)
     add_param('learning_rate', 1., ['train'], False)
   else:
     add_param('learning_rate', 1., ['train'], True)
   
   train_settings += [
     ('cg_damping', 1e-5),
-    ('cg_iters', 10),
   ]
+  add_param('cg_iters', 0, ['train'], True)
+  #add_param('cg_iters', 10, ['train'], False)
+  add_param('optimizer', 'Adam', ['train'], True)
 else:
   add_param('learning_rate', 1e-5 if recurrent else 1e-4, ['train'], True)
-  add_param('optimizer', 'Adam', ['train'])
+  add_param('optimizer', 'Adam', ['train'], False)
+
+#if recurrent:
+#  add_param('clip', 0.05, ['train'])
 
 for k, v in train_settings:
   add_param(k, v, ['train'], False)
 
+# embed params
+
+add_param('xy_scale', 0.05, both, False)
+#add_param('speed_scale
+
 #add_param('action_space', 0, both)
-#add_param('player_space', 0, both, True)
+add_param('player_space', 0, both, True)
 
 #add_param('critic_layers', [128, 128, 128], both)
-#add_param('actor_layers', [128, 128], both)
+#add_param('actor_layers', [128, 128, 128], both)
 
 # agent settings
 
 add_param('dolphin', True, ['agent'], False)
 
-add_param('experience_time', 20, both, False)
+add_param('experience_time', 1 if recurrent else 5, both, False)
+add_param('reload', 10, ['agent'], False)
 add_param('act_every', 3, both, False)
 
 delay = 2
@@ -122,19 +138,21 @@ char = 'marth'
 #char = 'puff'
 add_param('char', char, ['agent'], True)
 
-enemies = "easy"
+#enemies = "easy"
+#enemies = "delay0"
+enemies = "delay%d" % delay
 add_param('enemy_reload', 600, ['agent'], False)
 
 exp_name += "_enemies_" + enemies
 job_dicts['enemies'] = enemies
 
 # number of agents playing each enemy
-agents = 60
+agents = 120
 job_dicts['agents'] = agents
 
 add_param('name', exp_name, both, False)
 path = "saves/%s/" % exp_name
-add_param('path', path, both, False)
+#add_param('path', path, both, False)
 
 print("Writing to", path)
 util.makedirs(path)
