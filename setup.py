49a82e0e4aeefd541ce51ef84b0db457261d21eb
Vlad Firoiu <vladfi2@gmail.com>
2017-02-16 15:06:48 -0800

    Install run.py as phillip.

--- setup.py
+++ setup.py
@@ -101,13 +101,15 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     #data_files=[('my_data', ['data/data_file'])],
+    
+    #scripts = ['phillip/run.py'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'phillip=run:run',
+            'phillip=run:main',
         ],
     },
 )
