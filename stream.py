17f96e0e7f3999ae2f6d2af2ea8a7834fee46a13
Vlad Firoiu <vladfi2@gmail.com>
2017-02-24 01:39:31 -0500

    stream sound as well

--- stream.py
+++ stream.py
@@ -5,13 +5,14 @@ stream_args = [
   '-video_size', '1366x768',
   '-framerate', '30',
   '-f', 'x11grab', '-i', ':0.0',
-  #'-f', 'pulse', '-ac', '2', '-i', 'default',
+  '-f', 'pulse', '-ac', '2', '-i', 'default',
   '-c:v', 'libx264', '-preset', 'veryfast',
   '-maxrate', '3000k', '-bufsize', '6000k',
   '-pix_fmt', 'yuv420p',  '-g', '50',
-  #'-c:a', 'aac', '-b:a', '160k', '-ar', '44100', '-threads', '0', '-strict', '-2',
+  '-c:a', 'mp3', '-b:a', '160k', '-ar', '44100', '-threads', '0', '-strict', '-2',
   '-f', 'flv',
   'rtmp://live.twitch.tv/app/' + twitch_key
+  #'test.flv'
 ]
 
 import subprocess
