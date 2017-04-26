368c36259d31d0a760a08c6f33e0b2e0ef670af4
Vlad Firoiu <vladfi2@gmail.com>
2017-04-26 18:24:11 -0400

    Smooth over all n-step returns (generalized advantage estimation).

--- phillip/critic.py
+++ phillip/critic.py
@@ -8,7 +8,7 @@ class Critic(Default):
   _options = [
     Option('critic_layers', type=int, nargs='+', default=[128, 128]),
     Option('critic_learning_rate', type=float, default=1e-4),
-    Option('gae_lamdba', type=float, default=1., help="Generalized Advantage Estimation"),
+    Option('gae_lambda', type=float, default=1., help="Generalized Advantage Estimation"),
     Option('fix_scopes', type=bool, default=False),
   ]
   
@@ -48,28 +48,32 @@ class Critic(Default):
 
     values = tf.squeeze(self.net(history), [-1])
     trainVs = values[:,:-1]
-    lastV = values[:,-1]
+    # lastV = values[:,-1]
     
     rewards = reward[:,self.rlConfig.memory:]
-    deltaVs = reward + self.rlConfig.discount * values[:,1:] - trainVs
+    deltaVs = rewards + self.rlConfig.discount * values[:,1:] - trainVs
     
-    # errorVs = 
-    
-    # TODO: implement GAE, or some TD(N) weighting scheme
-    targets = tfl.discount2(rewards, self.rlConfig.discount, lastV)
+    advantages = tfl.discount2(deltaVs, self.rlConfig.discount * self.gae_lambda)
+
+    targets = trainVs + advantages
+    # targets = tfl.discount2(rewards, self.rlConfig.discount, lastV)
     targets = tf.stop_gradient(targets)
     
-    advantages = targets - trainVs
+    # advantages = targets - trainVs
     advantage_avg = tf.reduce_mean(advantages)
     tf.scalar_summary('advantage_avg', advantage_avg)
-    tf.scalar_summary('advantage_std', tf.sqrt(tf.reduce_mean(tf.squared_difference(advantages, advantage_avg))))
+    tf.scalar_summary('advantage_std', tf.sqrt(tfl.sample_variance(advantages)))
     
     vLoss = tf.reduce_mean(tf.square(advantages))
     tf.scalar_summary('v_loss', vLoss)
     tf.scalar_summary("v_uev", vLoss / tfl.sample_variance(targets))
     
     opt = tf.train.AdamOptimizer(self.critic_learning_rate)
-    train_op = opt.minimize(vLoss, var_list=self.variables)
+
+    # need to negate gradients since the optimizer negates again
+    grads = tf.gradients(trainVs, self.variables, grad_ys=-advantages)
+    train_op = opt.apply_gradients(zip(grads, self.variables))
+    # train_op = opt.minimize(vLoss, var_list=self.variables)
     
     return train_op, targets, advantages
 
