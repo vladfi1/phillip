f3eefe64fad75d75a005d57ebef264aa5f8d690a
Vlad Firoiu <vladfi2@gmail.com>
2017-12-28 22:36:12 -0500

    Don't depend on tensorflow to allow tensorflow-gpu to be used instead.

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['attrs', 'pyzmq', 'tensorflow'],
+    install_requires=['attrs', 'pyzmq'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
