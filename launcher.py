d7ffa1a3a2cbba3c3a80147492047a3941c7d2c7
Vlad Firoiu <vladfi2@gmail.com>
2017-12-30 11:54:41 -0500

    Run on 1080TIs as well.

--- launcher.py
+++ launcher.py
@@ -95,10 +95,9 @@ def launch(name, command, cpus=2, mem=1, gpu=False, log=True, qos=None, array=No
     #f.write("#SBATCH --cpu_bind=threads\n")
     opt("--partition=om_all_nodes,om_test_nodes")
     if gpu:
-      if args.any_gpu:
-        f.write("#SBATCH --gres gpu:1\n")
-      else:
-        f.write("#SBATCH --gres gpu:titan-x:1\n")
+      f.write("#SBATCH --gres gpu:1\n")
+      if not args.any_gpu:  # 31-54 have titan-x, 55-66 have 1080ti
+        f.write("#SBATCH -x node[001-030]\n")
     if qos:
       f.write("#SBATCH --qos %s\n" % qos)
     if array:
