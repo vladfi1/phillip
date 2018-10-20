0b166a059c94e669cad0c5d7038237390b1e542c
Vlad Firoiu <vladfi2@gmail.com>
2018-10-20 03:23:11 +0100

    Don't spam so hard.

--- phillip/cpu.py
+++ phillip/cpu.py
@@ -46,7 +46,7 @@ class CPU(Default):
     def __init__(self, **kwargs):
         Default.__init__(self, **kwargs)
 
-        self.toggle = False
+        self.toggle = 0
 
         self.user = os.path.expanduser(self.user)               
 
@@ -209,6 +209,7 @@ class CPU(Default):
             f.write('\n'.join(self.sm.locations()))
 
     def advance_frame(self):
+        # print("advance_frame")
         last_frame = self.state.frame
         
         self.update_state()
@@ -234,17 +235,16 @@ class CPU(Default):
         for message in messages:
           self.sm.handle(self.state, *message)
     
-    def spam(self, button):
-        if self.toggle:
+    def spam(self, button, period=120):
+        self.toggle = (self.toggle + 1) % period
+        if self.toggle == 0:
             self.pads[0].press_button(button)
-            self.toggle = False
-        else:
+        elif self.toggle == 1:
             self.pads[0].release_button(button)
-            self.toggle = True
     
     def make_action(self):
-        # menu = Menu(self.state.menu)
-        # print(menu)
+        #menu = Menu(self.state.menu)
+        #print(menu)
         if self.state.menu == Menu.Game.value:
             self.game_frame += 1
             
