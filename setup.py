83805ae914b1c933ce39bfb4dce777fe7b05c13b
Vlad Firoiu <vladfi2@gmail.com>
2024-12-29 21:02:37 -0500

    Set required tensorflow and python versions.

--- setup.py
+++ setup.py
@@ -78,7 +78,13 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['attrs', 'pyzmq', 'tensorflow'],
+    install_requires=[
+        'attrs',
+        'pyzmq',
+        'tensorflow==2.14',  # newer tensorflows don't work due to keras
+    ],
+
+    python_requires='<3.12',  # 3.12 drops support for tensorflow<2.16
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -102,7 +108,7 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     #data_files=[('my_data', ['data/data_file'])],
-    
+
     #scripts = ['phillip/run.py'],
 
     # To provide executable scripts, use entry points in preference to the
